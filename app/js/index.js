c00433d22888c31b37627f009a20185bba6e927c
Christine Lau <chylau.design@gmail.com>
2016-05-18 19:21:24 -0700

    got the modal to work with each project

--- app/js/index.js
+++ app/js/index.js
@@ -18,30 +18,81 @@ $(document).ready(function(){
     API.open();
   });
 
-$('nav li a').click(function(){
-  console.log("YOO");
-});
 //
+var modalOptions = [
+  {
+    modalTarget: "aModal1",
+    color: "#FFFFFF",
+    animatedIn: "bounceInUp",
+    animatedOut: "bounceOutDown",
+    animationDuration: "1s",
+    overflow: "scroll"
+  },
+  {
+    modalTarget: "aModal2",
+    color: "#FFFFFF",
+    animatedIn: "bounceInUp",
+    animatedOut: "bounceOutDown",
+    animationDuration: "1s",
+    overflow: "scroll"
+  },
+  {
+    modalTarget: "aModal3",
+    color: "#FFFFFF",
+    animatedIn: "bounceInUp",
+    animatedOut: "bounceOutDown",
+    animationDuration: "1s",
+    overflow: "scroll"
+  },
+  {
+    modalTarget: "aModal4",
+    color: "#FFFFFF",
+    animatedIn: "bounceInUp",
+    animatedOut: "bounceOutDown",
+    animationDuration: "1s",
+    overflow: "scroll"
+  },
+  {
+    modalTarget: "aModal5",
+    color: "#FFFFFF",
+    animatedIn: "bounceInUp",
+    animatedOut: "bounceOutDown",
+    animationDuration: "1s",
+    overflow: "scroll"
+  }
+
+];
+
+
+$("#modal01").animatedModal(modalOptions[0]);
+$("#modal02").animatedModal(modalOptions[1]);
+$("#modal03").animatedModal(modalOptions[2]);
+$("#modal04").animatedModal(modalOptions[3]);
+$("#modal05").animatedModal(modalOptions[4]);
+
 
 //SMOOTH SCROLL:
-$('a[href*="#"]:not([href="#"])').click(function() {
-   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-     var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-     if (target.length) {
-       $('html, body').animate({
-         scrollTop: target.offset().top
-       }, 2000, function(){
-         API.close();
-       });
-       return false;
-     }
-   }
- });
+$('li a[href*="#"]:not([href="#"])').click(function(e) {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000, function(){
+          API.close();
+        });
+        return false;
+      }
+    }
+  });
+
+
+
 
 
 
 console.log("READY");
 
 
-});
+});//END OF document.ready
