ae8e3afb0c7614fdee414d0fa208253fd4fce4e4
Christine Lau <chylau.design@gmail.com>
2016-05-27 15:14:29 -0700

    finished with adding images, but also finishing it up with the oneApp data obj

--- app/js/data.js
+++ app/js/data.js
@@ -1,16 +1,128 @@
+var bt = "images/bt/";
+var style = "images/stylem/";
+var one = "images/oneApp/";
+
 var projectData = [
   {
     name: "Boutiq.travel",
     github: "",
     website: "",
-    currentProj: false
-  },
+    currentProj: false,
+    coverImage: bt+"bt_cove.svg",
+    images: [
+      {
+        img: bt+"bt_iphone_home.svg",
+        size:"",
+        caption: "Home Page",
+        subCaption: "Landing Page for Boutiq.Travel.  One click to Log in to Facebook Authentication",
+      },
+      {
+        img: bt+"bt_iphone2.svg",
+        size:"",
+        caption:"New Feed",
+        subCaption:"New Feed has the latest reviews of your friends from Facebook. Instagram-like functionality.  You can like each review that has been posted",
+      },
+      {
+        img: bt+"bt_iphone3.svg",
+        size:"",
+        caption: "Post A Review Form",
+        subCaption: "Posting a review for all your friends to see.  Includes GoogleMaps Autocomplete, and GoogleMaps Api to find all the Places you want to review for your travels.",
+      },
+      {
+        img: bt+"bt_iphone4_profile_bg.svg",
+        size:"",
+        caption: "User's Profile Page",
+        subCaption:"Profile Page that includes all your reviews, ordered by the Latest review.  Your facebook friends can also post a comment on the Review you posted.",
+      },
+      {
+        img: bt+"bt_iphone5_search.svg",
+        size:"",
+        caption: "Searching for Places to check on?",
+        subCaption:"When you're traveling to a particular place, search for it and filter the results by TAGs; Where to Eat/ Drink/ Sleep/ Do.",
+      },
+      {
+        img: bt+"bt_search_iphone.svg",
+        size:"",
+        caption: "Filtered!",
+        subCaption:"Filtered by the 'Eat' tag, and how many reviews your friends already Reviewed",
+      },
+//Add the Place Profile page, and the Likes feature
+    ],
+  responsibilities: [],
+  futureFeatures: []
+
+
+  }, // End of Boutiq.travel
 
   {
     name: "Style Me",
     github: "",
     website:"",
-    currentProj: true
+    currentProj: true,
+    coverImage: style + "stylem_imac1.svg",
+    images: [
+      {
+        img: style + "style_3.svg",
+        size:"",
+        caption: "Styling Platform",
+        subCaption: "Given the User's current location, we get the current temperature and get a randomized selection of clothes based on how hot or how cold it is.  The User gets to choose the items he/she can style",
+      },
+      {
+        img: style + "style_1.svg",
+        size:"",
+        caption: "Style Page",
+        subCaption:"Saving your outfits that you curated for each day. It shows the temperature, the date, and the pictures of the outfits you kept",
+      },
+      {
+        img: style + "style_2.svg",
+        size:"",
+        caption: "Show Info",
+        subCaption:"Gives you information on what brands and stores you can find these items",
+      },
+      //Want to add Men's style also.
+    ],
+    responsibilities: [],
+    futureFeatures: []
+    //Add that you would want the images to click to the store name
+    //Add that you would want to update the algorithm
+    //add that you would want to turn it into a Rangular App
+  }, // End of StyleMe
+
+  {
+    name: "The ONE APP",
+    github: "",
+    website: "",
+    currentProj: false,
+    coverImage: one + "one_app_cover.svg",
+    images: [
+      {
+        img: one+"one_app_full.svg",
+        size:"full",
+        caption: "By Catagory or Artist",
+        subCaption:"The Artwork is catagorized by Medium, or by the Artist themselves.  Here we have it organized by Illustration",
+      },
+      {
+        img: one+"one_app_full2.svg",
+        size:"full",
+        caption: "Art Piece",
+        subCaption:"Each piece of art can be added your cart and be added to your collection.",
+      },
+      {
+        img: one+"one_app_full3.svg",
+        size:"full",
+        caption: "Collection",
+        subCaption:"The Collection feature was built to save a user's favorites and admire the pieces that the love.  They can remove it by hovering a piece and pressing the remove button.",
+      },
+      {
+        img: one + "one_app_1.svg",
+        size: "half",
+        caption: "Shopping Cart",
+        subCaption: "Buying a piece(s) has never been easier!  Add the pieces into your cart and the Stripe API will check each customer out.",
+      }
+//One more pic of the shopping cart in the imac feature
+    ],
+  responsibilities: [],
+  futureFeatures: []
 
   },
 
@@ -18,20 +130,139 @@ var projectData = [
     name: "Friends Jeopardy",
     github:"",
     website: "",
-    currentProj: false
+    currentProj: false,
+    coverImage: "",
+    images: [
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      }
+    ],
+  responsibilities: [],
+  futureFeatures: []
+
   },
 
-  {
-    name: "The ONE APP",
-    github: "",
-    website: "",
-    currentProj: false
-
-  },
   {
     name: "Portfolio",
     github: "",
     website: "",
-    currentProj: false
+    currentProj: false,
+    coverImage: "",
+    images: [
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      },
+      {
+        img: "",
+        size:"",
+        caption: "",
+        subCaption:"",
+      }
+    ],
+    responsibilities: [],
+    futureFeatures: []
+
   }
 ];// end of projectData
